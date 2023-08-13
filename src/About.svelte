53139ee0caba01b61b5d8a47d13f3d28844d4571
unoptimal <unoptimalblogs@gmail.com>
2023-08-13 10:39:13 -0400

    stuff

---
+++ src/About.svelte
@@ -0,0 +1,181 @@
+<script>
+  export let show = false;
+  export let onClose;
+</script>
+
+{#if show}
+<div class="modal">
+    <div class="modal-content">
+        <div class="modal-columns">
+
+            <!-- Left Column -->
+            <div class="left-column">
+                <!-- Banner and Title -->
+                <div class="top-content">
+                    <img class="logo" src="../src/assets/desktop.svg" alt="Wider Pringles Logo">
+                </div>
+                <!-- Description for Mobile -->
+                <div class="description mobile-desc">
+                    <p>A 10" x 5" x 5" can. Your hand is almost guaranteed to fit.</p>
+                    <p>Unfortunately, this project is not affiliated with Pringles or Kellog's in any way.</p>
+                    <small>by <a href='https://unoptimal.com/' target='_blank'>unoptimal</a></small>
+                    <br>
+                    <button class="close-button" on:click={onClose}>Close</button>
+                </div>
+            </div>
+
+            <!-- Right Column for Images -->
+            <div class="right-column">
+                <div class="image" style="background-image: url('../src/assets/three.webp')"></div>
+                <div class="image" style="background-image: url('../src/assets/in.webp')"></div>
+                <div class="image" style="background-image: url('../src/assets/compare.webp')"></div>
+            </div>  
+
+            <!-- Description for Desktop -->
+            <div class="description desktop-desc">
+                <p>A 10" x 5" x 5" can. Your hand is almost guaranteed to fit.</p>
+                <p>Unfortunately, this project is not affiliated with Pringles or Kellog's in any way.</p>
+                <small>by <a href='https://unoptimal.com/' target='_blank'>unoptimal</a></small>
+                <br>
+                <button class="close-button" on:click={onClose}>Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+{/if}
+
+<style>
+p {
+    font-size: 1.5rem;
+}
+
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 20;
+}
+
+.modal-content {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    max-width: 1000px;
+    width: 90%;
+    height: 75%;
+    display: flex;
+    flex-direction: column;
+}
+
+.modal-columns {
+    flex: 1;
+    display: flex;
+    gap: 20px;
+    width: 100%;
+    flex-direction: row; 
+}
+
+.top-content {
+    text-align: center;
+}
+
+.left-column, .right-column {
+    flex: 1;
+}
+
+.left-column {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+}
+
+.logo {
+    max-width: 100%;
+    height: auto;
+}
+
+.right-column {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    overflow-y: auto;
+    overflow-x: hidden; 
+    max-height: 60%; 
+}
+
+.image {
+    min-height: 300px;
+    min-width: 300px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.close-button {
+    background-color: #f1f1f1;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.close-button:hover {
+    background-color: #ddd;
+}
+
+.desktop-desc {
+    display: none;
+}
+
+@media (max-width: 768px) {
+
+  .left-column, .right-column {
+        flex: none; 
+    }
+
+  p {
+        font-size: 1.25rem;
+    }
+
+    .modal-content {
+        height: 85%;
+        width: 85%;
+    }
+
+    .modal-columns {
+        flex-direction: column;
+    }
+
+    .right-column {
+        display: flex;
+        flex-direction: row;
+        overflow-x: auto;
+        gap: 10px;
+        white-space: nowrap;
+    }
+
+    .image {
+        flex: 0 0 auto;
+        width: 100%;
+        max-width: 400px;
+        height: auto;
+        min-height: 250px;
+        background-size: contain;
+        overflow-x: hidden;
+    }
+
+    .desktop-desc {
+        display: block;
+    }
+
+    .mobile-desc {
+        display: none;
+    }
+}
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<script>
+  import About from "./About.svelte";
+
+  let showAbout = false;
+  function toggleAbout() {
+      showAbout = !showAbout;
+  }
+</script>
+
+<div class="container">
+  <div class='more'>
+      <button on:click={toggleAbout}>MORE</button>
+      <About show={showAbout} onClose={toggleAbout} />
+  </div>
+  <div class="video-wrapper">
+    <iframe src="https://www.youtube.com/embed/4TBWZnceI9o" title="Introducing: Wider Pringles Cans" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; margin: 0;"></iframe>
+  </div>
+</div>
+
+<style>
+  :global(body), :global(html) {
+    
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background-size: cover; /* Make the image cover the entire element */
+    background-repeat: no-repeat; /* Prevent the image from repeating */
+    background-position: center center; /* Center the image */
+    overflow: hidden;
+
+  }
+
+  :global(body) {
+    background-image: url('../src/assets/BG.webp');
+}
+
+
+  /* Box-sizing reset */
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center; /* vertically centers content */
+    align-items: center; /* horizontally centers content */
+    height: auto;
+    min-height: 100vh;
+}
+
+  .video-wrapper {
+  position: absolute;
+  top: 10%;       /* Adjust this value to position it vertically */
+  right: 10%;     /* Adjust this value to position it horizontally */
+  height: 40%;
+  width: 40%;    /* Width of the video, can adjust based on design */
+  padding-bottom: 16.875%;  /* This ensures a 16:9 ratio: 30% * 9/16 */
+  overflow: hidden; 
+  z-index: 10;
+ 
+}
+
+.video-wrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+
+.more{
+  position: absolute;
+  top: 90%;
+  left: 68%;
+  padding-bottom: 16.875%;
+  overflow: hidden;
+}
+
+  /* Mobile styles */
+  @media (max-width: 768px) {
+    :global(body) {
+      background-image: url('../src/assets/MOBILEBG.webp');
+    }
+
+    .video-wrapper {
+      top: 10%;  /* Adjust this value for mobile */
+      right: 10%;  /* Adjust this value for mobile */
+      height: 25%;  /* Adjust this value for mobile */
+      width: 80%;  /* Adjust this value for mobile */
+    }
+
+    .more{
+  position: absolute;
+  top: 92.5%;
+  left: 75%;
+  padding-bottom: 16.875%;
+  overflow: hidden;
+}
+  }
+</style>
+
+
